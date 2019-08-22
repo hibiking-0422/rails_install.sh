2d94e49f91cb8592e3b5acc182b5203cb5ac7d6e
hibiking-0422 <50702357+hibiking-0422@users.noreply.github.com>
2019-08-23 01:26:09 +0900

    config_add

--- rails_install.sh
+++ rails_install.sh
@@ -40,7 +40,7 @@ Vagrant.configure("2") do |config|
 
   # Create a private network, which allows host-only access to the machine
   # using a specific IP.
-  config.vm.network "private_network", ip: "192.168.33.10"
+  config.vm.network "forwarded_port", guest: 80, host: 8080, host_ip: "127.0.0.1"
 
   # Create a public network, which generally matched to bridged network.
   # Bridged networks make the machine appear as another physical device on
