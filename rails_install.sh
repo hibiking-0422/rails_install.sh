e6744a8eff0c28a6d5e2a9a79bc38e885f3d14f7
hibiking-0422 <50702357+hibiking-0422@users.noreply.github.com>
2019-09-12 11:46:25 +0900

    ヴぁｓｖｃ
    vasvcasvあ

--- rails_install.sh
+++ rails_install.sh
@@ -31,16 +31,18 @@ sudo yum install -y openssl-devel readline-devel zlib-devel
 sudo yum install -y gcc-c++
 sudo yum -y install bzip2
 
-#rbenv install
-git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
-echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
-source ~/.bash_profile
+
 
 #ruby-build install
 git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 cd ~/.rbenv/plugins/ruby-build
 sudo ./install.sh
+
+#rbenv install
+git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.zshrc
+echo 'eval "$(rbenv init -)"' >> ~/.zshrc
+source ~/.zshrc
 
 #needed ruby pachage
 sudo yum install -y openssl-devel readline-devel zlib-devel
